15bd09b07c931e99a3260837f063e0c26d06caac
daniel_prince <danikoumba7@gmail.com>
2025-12-21 10:42:20 +0100

    delete import elasticsearch

--- app/extension.py
+++ app/extension.py
@@ -2,10 +2,9 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-from elasticsearch import Elasticsearch
+
 
 db=SQLAlchemy()
 migrate=Migrate()
 jwt=JWTManager()
 cors=CORS()
-es=Elasticsearch('http://localhost:9200')
