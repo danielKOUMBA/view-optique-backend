3ac7982fa1267d3ab8cf3a7db7fa2be4fdb95cec
daniel_prince <danikoumba7@gmail.com>
2025-12-21 12:47:13 +0100

    db

--- app/api/test_db.py
+++ app/api/test_db.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify,Blueprint
-from app.extension import db  # ton import SQLAlchemy
+from app.extension import db  # ton objet SQLAlchemy
 
+db_bp = Blueprint("db_bp", __name__)
 
-db_bp = Blueprint('db_bp', __name__)
 @db_bp.route("/test-db")
 def test_db():
     try:
-        # Simple requête pour tester la connexion
-        result = db.engine.execute("SELECT 1").fetchone()
-        return jsonify({"db_connected": True, "result": result[0]})
+        # Crée une connexion avec le context manager
+        with db.engine.connect() as conn:
+            result = conn.execute("SELECT 1")
+            first = result.scalar()  # récupère la première valeur
+        return jsonify({"db_connected": True, "result": first})
     except Exception as e:
         return jsonify({"db_connected": False, "error": str(e)})
