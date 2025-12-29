8fd8252bbe7b31fdd9b50a51a2e0d04d882d73bc
daniel_prince <danikoumba7@gmail.com>
2025-12-29 14:19:27 +0100

    production

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(config_class=Config):
     with app.app_context():
         db.create_all()
     migrate.init_app(app,db)
-    cors.init_app(app,supports_credentials=True,origins=['https://view-optique-vision.vercel.app/'])
+    cors.init_app(app,supports_credentials=True,origins=['https://view-optique-vision.vercel.app'])
     jwt.init_app(app)
     if app.config.get('TESTING'):
         return app
